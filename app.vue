fb13334099f15ab8c2c96efe773dc65845cf8378
mwllms <mrkwillems@gmail.com>
2022-11-30 13:33:43 +0100

    Feature: setup firebase messaging

--- app.vue
+++ app.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+  import { getToken, onMessage } from 'firebase/messaging'
+  const { $firebaseMessaging } = useNuxtApp()
+
+  onMounted(() => {
+    onMessage($firebaseMessaging, (payload) => {
+      console.log('Message on client', payload)
+    })
+  })
+
+  if (process.client) {
+    const token = await getToken($firebaseMessaging, {
+      vapidKey: 'BDV4FE9CMx--v_5gQRsqMt0L4X9p17FBAsFFwEgaj60VingYcX7aNoAcwBNMTvtYqCUHP86BsOL9sg2Jp1dhILQ',
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          // Send the token to your server and update the UI if necessary
+          // ...
+          return currentToken
+        } else {
+          // Show permission request UI
+          console.log('No registration token available. Request permission to generate one.')
+          // ...
+        }
+      })
+      .catch((err) => {
+        console.log('An error occurred while retrieving token. ', err)
+        // ...
+      })
+    console.log(token)
+  }
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import { getMessaging } from 'firebase/messaging'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,12 +20,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
+const messaging = getMessaging(app)
 
 export default defineNuxtPlugin((NuxtApp) => {
   return {
     provide: {
       firebaseApp: app,
       firebaseAnalytics: analytics,
+      firebaseMessaging: messaging,
     },
   }
 })
