649338ae6485b02b3542151066318f4b1e86ba05
mwllms <mrkwillems@gmail.com>
2022-12-02 09:18:22 +0100

    Feature: setup app manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'manifest',
+          href: './manifest.json',
+        },
+      ],
+    },
+  },
 })
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 // Scripts for firebase and firebase messaging
 importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js')
 
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
@@ -28,3 +29,48 @@ messaging.onBackgroundMessage(function (payload) {
 
   self.registration.showNotification(notificationTitle, notificationOptions)
 })
+
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => self.clients.claim())
+
+const { registerRoute } = workbox.routing
+const { NetworkFirst, StaleWhileRevalidate, CacheFirst } = workbox.strategies
+const { CacheableResponsePlugin } = workbox.cacheableResponse
+const { ExpirationPlugin } = workbox.expiration
+
+// Cache page navigations (html) with a Network First strategy
+registerRoute(
+  ({ request }) => {
+    return request.mode === 'navigate'
+  },
+  new NetworkFirst({
+    cacheName: 'pages',
+    plugins: [new CacheableResponsePlugin({ statuses: [200] })],
+  })
+)
+
+// Cache Web Manifest, CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
+registerRoute(
+  ({ request }) =>
+    request.destination === 'manifest' ||
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'worker',
+  new StaleWhileRevalidate({
+    cacheName: 'assets',
+    plugins: [new CacheableResponsePlugin({ statuses: [200] })],
+  })
+)
+
+// Cache images with a Cache First strategy
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new CacheableResponsePlugin({ statuses: [200] }),
+      // 50 entries max, 30 days max
+      new ExpirationPlugin({ maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 30 }),
+    ],
+  })
+)
